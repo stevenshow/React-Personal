b0ff7e746242e5599a3fbd8ac3ae1107fbb0beec
Steven Schoebinger <steven.schoebinger@gmail.com>
2021-10-26 15:03:26 -0600

    Added links for github and linkedin component

---
+++ src/components/Links.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+import gitLogo from "../images/github.png";
+import linkedLogo from "../images/linkedin.png";
+
+import "./Links.css";
+
+class Links extends React.Component {
+  render() {
+    return (
+      <div>
+        <a
+          className="link"
+          href="https://github.com/stevenshow"
+          target="_blank"
+          onClick
+        >
+          <img src={gitLogo} height="30" width="30" />
+        </a>
+        <span>stevenshow</span>
+        <a
+          className="link"
+          href="https://www.linkedin.com/in/steven-schoebinger/"
+          target="_blank"
+          onClick
+        >
+          <img src={linkedLogo} height="30" width="30" />
+        </a>
+        <span>steven-schoebinger</span>
+      </div>
+    );
+  }
+}
+
+export default Links;
