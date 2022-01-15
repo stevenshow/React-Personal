ff7b2b15b3eb0b17f6ae4ad3dd432adec2a0630c
Steven Schoebinger <Steven.schoebinger@churchofjesuschrist.org>
2022-01-14 23:33:39 -0700

    Added projects page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
+import Projects from "./pages/Projects.jsx";
 
 function App() {
   return (
     <Router>
       <div>
         <Route exact path="/" component={Home} />
+        <Route path="/projects" component={Projects} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
       </div>
